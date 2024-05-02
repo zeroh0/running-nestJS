986e4030dfecfc04cc105d32db21e3caaab0958f
zeroh0 <umberto1900@naver.com>
2024-05-03 04:50:31 +0900

    Feat: 게시글 전체 조회

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { Board } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class BoardService {
+  constructor(private prisma: PrismaService) {}
+
   create(createBoardDto: CreateBoardDto) {
     return 'This action adds a new board';
   }
 
-  findAll() {
-    return `This action returns all board`;
+  async findAll(): Promise<Board[]> {
+    return this.prisma.board.findMany();
   }
 
   findOne(id: number) {
