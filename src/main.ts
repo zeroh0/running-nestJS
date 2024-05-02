5cdeef7f1c50266a34159223d914822c75bec39a
zeroh0 <umberto1900@naver.com>
2024-05-03 04:17:38 +0900

    Add: ValidationPipe 추가 및 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 유효하지 않은 속성 자동 제거
+      whitelist: true,
+      // 전송된 데이터 자동으로 타입 변환
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
