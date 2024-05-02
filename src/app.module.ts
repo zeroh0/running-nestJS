6ec01092e880598edeb39eea25b5acc57d1a9e4f
zeroh0 <umberto1900@naver.com>
2024-05-03 04:00:14 +0900

    Add: generate board CRUD resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
+  imports: [BoardModule],
   controllers: [],
   providers: [],
 })
